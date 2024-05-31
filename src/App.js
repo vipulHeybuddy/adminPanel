f6513c8629511e2cbd58312e6fba117cae8ba3eb
vipulHeybuddy <149054269+vipulHeybuddy@users.noreply.github.com>
2024-05-31 10:58:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import FormComponent from './FormComponent';
+function App() {
+  return (
+    <div className="App">
+     <FormComponent />
+    </div>
+  );
+}
+
+export default App;
